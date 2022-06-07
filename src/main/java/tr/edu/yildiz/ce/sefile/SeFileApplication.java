bfadbdd4b063d3398e4e8cdb6f16c36a460bdcf3
cflv5 <bedirhanakcay@icloud.com>
2022-06-07 15:35:30 +0300

    initial commit

---
+++ src/main/java/tr/edu/yildiz/ce/sefile/SeFileApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.yildiz.ce.sefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/yildiz/ce/sefile/SeFileApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.yildiz.ce.sefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
