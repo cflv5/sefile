ec2883639ed2fdd4a6ffafcaa028a75fbf363b00
cflv5 <bedirhanakcay@icloud.com>
2022-06-07 15:51:50 +0300

    feat: scan sebase package

--- src/main/java/tr/edu/yildiz/ce/sefile/SeFileApplication.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/SeFileApplication.java
@@ -2,8 +2,10 @@ package tr.edu.yildiz.ce.sefile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "tr.edu.yildiz.ce.se.base", "tr.edu.yildiz.ce.sefile" })
 public class SeFileApplication {
 
 	public static void main(String[] args) {
