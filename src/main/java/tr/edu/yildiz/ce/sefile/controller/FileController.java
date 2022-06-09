413cb514ebb76b1932274f289134cccbaa1c086a
cflv5 <bedirhanakcay@icloud.com>
2022-06-09 23:51:04 +0300

    feat: add endpoint to get a file

--- src/main/java/tr/edu/yildiz/ce/sefile/controller/FileController.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/controller/FileController.java
@@ -6,7 +6,11 @@ import java.security.NoSuchAlgorithmException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -32,6 +36,16 @@ public class FileController {
             @RequestPart("properties") @Valid FileInsertControllerRequest request,
             @RequestPart("file") MultipartFile file) throws NoSuchAlgorithmException, IOException {
         return ResponseEntity.ok().body(fileControllerService.insertFile(request, file));
+    }
+
+    @GetMapping(value = "/content/{id}")
+    public ResponseEntity<Resource> fetchFileContent(@PathVariable(value = "id") String id) throws IOException {
+        var resource = fileControllerService.fetchFile(id);
+        return ResponseEntity
+                .ok()
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
 }
