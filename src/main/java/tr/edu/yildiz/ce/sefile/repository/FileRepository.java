76876674e9e8e5fd92c2216a39c2258e92234baa
cflv5 <bedirhanakcay@icloud.com>
2022-06-09 22:57:06 +0300

    fix: import correct File class

--- src/main/java/tr/edu/yildiz/ce/sefile/repository/FileRepository.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/repository/FileRepository.java
@@ -1,8 +1,8 @@
 package tr.edu.yildiz.ce.sefile.repository;
 
-import java.io.File;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import tr.edu.yildiz.ce.sefile.domain.entity.File;
 
 public interface FileRepository extends JpaRepository<File, String>{
 
