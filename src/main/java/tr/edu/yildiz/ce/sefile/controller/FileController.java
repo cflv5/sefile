76ae64e4ecfbc019b1ade64d76712e9f878df742
cflv5 <bedirhanakcay@icloud.com>
2022-06-09 22:58:59 +0300

    feat: add endpoint to insert file

--- src/main/java/tr/edu/yildiz/ce/sefile/controller/FileController.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/controller/FileController.java
@@ -1,9 +1,20 @@
 package tr.edu.yildiz.ce.sefile.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import tr.edu.yildiz.ce.sefile.domain.request.FileInsertControllerRequest;
+import tr.edu.yildiz.ce.sefile.domain.response.FileInsertControllerResponse;
 import tr.edu.yildiz.ce.sefile.service.FileControllerService;
 
 @RestController
@@ -16,5 +27,11 @@ public class FileController {
         this.fileControllerService = fileControllerService;
     }
 
-    
+    @PostMapping(value = { "/", "" })
+    public ResponseEntity<FileInsertControllerResponse> insert(
+            @RequestPart("properties") @Valid FileInsertControllerRequest request,
+            @RequestPart("file") MultipartFile file) throws NoSuchAlgorithmException, IOException {
+        return ResponseEntity.ok().body(fileControllerService.insertFile(request, file));
+    }
+
 }
