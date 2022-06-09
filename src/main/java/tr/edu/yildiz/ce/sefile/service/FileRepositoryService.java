30418e957d40cfa3a656eb3a3d09f5a2e3bd8414
cflv5 <bedirhanakcay@icloud.com>
2022-06-09 23:50:38 +0300

    feat: add service method to fetch a file

--- src/main/java/tr/edu/yildiz/ce/sefile/service/FileRepositoryService.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/service/FileRepositoryService.java
@@ -1,7 +1,10 @@
 package tr.edu.yildiz.ce.sefile.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import tr.edu.yildiz.ce.se.base.context.TenantContext;
+import tr.edu.yildiz.ce.se.base.exception.SeBaseException;
 import tr.edu.yildiz.ce.sefile.domain.entity.File;
 import tr.edu.yildiz.ce.sefile.repository.FileRepository;
 
@@ -15,6 +18,16 @@ public class FileRepositoryService {
 
     public File saveFile(File file) {
         return fileRepository.save(file);
+    }
+
+    public File findFileWithId(String id) {
+        var optFile = fileRepository.findById(id);
+        
+        var file = optFile.orElseThrow(() -> new SeBaseException("File Not Found", HttpStatus.NOT_FOUND));
+
+        file.hasTenantRightToAccess(TenantContext.getCurrentTenant().getTenantId());
+
+        return file;
     }
 
 }
