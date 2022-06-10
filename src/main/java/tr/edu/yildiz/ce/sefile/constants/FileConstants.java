6d40d621f512e32b9f749009d16f7f61236d2106
cflv5 <bedirhanakcay@icloud.com>
2022-06-10 22:45:45 +0300

    feat: create constants class

---
+++ src/main/java/tr/edu/yildiz/ce/sefile/constants/FileConstants.java
@@ -0,0 +1,10 @@
+package tr.edu.yildiz.ce.sefile.constants;
+
+public final class FileConstants {
+    private FileConstants() {
+        throw new AssertionError("No instance allowed.");
+    }
+
+    public static final String UNAUTHORIZED = "Unauthorized";
+    
+}
