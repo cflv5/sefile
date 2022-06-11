b9d8df8b080075a894785fbd45e3e0a15adf59ad
cflv5 <bedirhanakcay@icloud.com>
2022-06-11 10:40:33 +0300

    feat: add hashValue field

--- src/main/java/tr/edu/yildiz/ce/sefile/domain/request/FileInsertControllerRequest.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/domain/request/FileInsertControllerRequest.java
@@ -1,12 +1,17 @@
 package tr.edu.yildiz.ce.sefile.domain.request;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import tr.edu.yildiz.ce.sefile.domain.entity.AccessType;
+import org.springframework.validation.annotation.Validated;
 
+import tr.edu.yildiz.ce.sefile.domain.entity.AccessType;
+@Validated
 public class FileInsertControllerRequest {
     @NotNull
     private AccessType accessType;
+    @NotBlank
+    private String hashValue;
 
     public AccessType getAccessType() {
         return accessType;
@@ -15,5 +20,13 @@ public class FileInsertControllerRequest {
     public void setAccessType(AccessType accessType) {
         this.accessType = accessType;
     }
-    
+
+    public String getHashValue() {
+        return hashValue;
+    }
+
+    public void setHashValue(String hashValue) {
+        this.hashValue = hashValue;
+    }
+
 }
