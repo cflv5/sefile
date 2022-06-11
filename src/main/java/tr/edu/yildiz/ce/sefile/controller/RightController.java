28a423453cc1f4578549a772a45f955d2b75b416
cflv5 <bedirhanakcay@icloud.com>
2022-06-11 12:21:40 +0300

    feat: add endpoint for right insert

--- src/main/java/tr/edu/yildiz/ce/sefile/controller/RightController.java
+++ src/main/java/tr/edu/yildiz/ce/sefile/controller/RightController.java
@@ -1,13 +1,19 @@
 package tr.edu.yildiz.ce.sefile.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tr.edu.yildiz.ce.sefile.domain.request.InsertFileRightControllerRequest;
 import tr.edu.yildiz.ce.sefile.domain.response.FetchPoliciesControllerResponse;
+import tr.edu.yildiz.ce.sefile.domain.response.InsertFileRightControllerResponse;
 import tr.edu.yildiz.ce.sefile.service.RightControllerService;
 
 @RestController
@@ -23,6 +29,12 @@ public class RightController {
     @GetMapping(value = "/file/{id}")
     public ResponseEntity<FetchPoliciesControllerResponse> fetchFileRight(@PathVariable(value = "id") String fileId) {
         return ResponseEntity.ok().body(rightControllerService.fetchRights(fileId));
+    }
+
+    @PostMapping(value = "/file/{id}")
+    public ResponseEntity<InsertFileRightControllerResponse> addFileRight(@PathVariable(value = "id") String fileId,
+            @RequestBody @Valid InsertFileRightControllerRequest request) {
+        return ResponseEntity.ok().body(rightControllerService.addRight(request, fileId));
     }
 
 }
